3aa6eb1bdc8e6b8f60c086abfea20fe74eb5fa49
Shreyash Poudel <poudelshreyash36@gmail.com>
2026-03-23 21:03:18 +0545

    Checks and Thresholds 🔥

--- K6_PROJ1/scripts/fisrt_k6_script.js
+++ K6_PROJ1/scripts/fisrt_k6_script.js
@@ -1,17 +1,39 @@
 import http from 'k6/http';
-import {sleep} from 'k6';
+import {sleep, check} from 'k6';
 // import {htmlReport} from "http://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+const BASE_URL = __ENV.BASE_URL || 'https://test.k6.io';
 
 // adding virtual users and duration of the test
 export const options = {
-    vus : 5,
-    duration : '30s'
+    vus : 3,
+    duration : '5s',
+
+    // thresholds -> defines performance limits
+    thresholds : {
+        http_req_duration : ['p(95)<200'], // 95% of the requests must finish under 500ms
+    }
+
 };
 
+// export default function(){
+// http.get('https://test.k6.io');
+// sleep(1);
+// }
+
 export default function(){
-http.get('https://test.k6.io');
-sleep(1);
+    const response = http.get(BASE_URL);
+    sleep(1);
+
+    check(response,{
+    // 'name of the' : response object => actual check (for example, checking the status code of the response)
+    'status is 200': (r) => r.status === 200, 
+
+})
 }
+
+
+
 
 
 
